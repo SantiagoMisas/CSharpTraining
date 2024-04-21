0cd1fac890be54ab40d3b0f3ba8143066dd9b672
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2024-04-21 18:11:30 -0500

    initial commit

---
+++ C# Training/C# Training/DataTypes.cs
@@ -0,0 +1,20 @@
+﻿/*string nombre = "Santiago M";
+string apellido = "Reyes";
+int a = 100;
+long b;
+float c = 50.70f;
+double d = 97.23;
+byte e = 130; // de 0 a 255
+decimal f = 478.674m; // Formato de moneda
+short g;
+char h = 'A';
+bool? i = null; // Puede ser true o false, lleva el ? para que acepte null
+// struct j; // No está inicializado
+// enum h; // No está inicializado
+string k = "cadena de texto";
+dynamic l;
+Array m;
+// delegate n; // No está inicializado
+const int constanteNumerica = 600;
+Console.WriteLine("Primeros cambios en proyecto " + nombre);
+*/
